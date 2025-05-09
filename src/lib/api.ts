02e1513136214544f30cf6c3bf259101f18eaceb
Rey Gienaldo Jonathan <83940099+j-reygienaldo@users.noreply.github.com>
2025-05-10 03:28:38 +0700

    Add api for file

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,3 +11,61 @@ export async function getSubfolder(parentId: number) {
 
   return res.json();
 }
+
+export async function createFolder(name: string, parentId: number | null) {
+  const res = await fetch("http://127.0.0.1:4000/api/v1/folders/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, parent_id: parentId }),
+  });
+  return res.json();
+}
+
+export async function updateFolder(id: number, name: string) {
+  const res = await fetch(`http://127.0.0.1:4000/api/v1/folders/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name }),
+  });
+  return res.json();
+}
+
+export async function deleteFolder(id: number) {
+  const res = await fetch(`http://127.0.0.1:4000/api/v1/folders/${id}`, {
+    method: "DELETE",
+  });
+  return res.json();
+}
+
+export async function createFile(
+  name: string,
+  folderId: number | null,
+  type: string
+) {
+  const res = await fetch("http://127.0.0.1:4000/api/v1/file", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, folder_id: folderId, type }),
+  });
+  return res.json();
+}
+
+export async function updateFile(
+  id: number,
+  name: string,
+  type: string | null
+) {
+  const res = await fetch(`http://127.0.0.1:4000/api/v1/file/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, type }),
+  });
+  return res.json();
+}
+
+export async function deleteFile(id: number) {
+  const res = await fetch(`http://127.0.0.1:4000/api/v1/file/${id}`, {
+    method: "DELETE",
+  });
+  return res.json();
+}
